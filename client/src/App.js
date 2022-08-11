d2db4778d7cf1ba90fd5c7086a423ae178e0f007
sophiahdew <59624772+sophiahdew@users.noreply.github.com>
2022-08-11 17:07:04 -0400

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+import AllowlistContract from "./contracts/Allowlist.json";
+import getWeb3 from "./getWeb3";
+import AllowlistForm from "./components/AllowlistForm";
+
+import "./App.css";
+
+class App extends Component {
+  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+
+  componentDidMount = async () => {
+    try {
+      // Get network provider and web3 instance.
+      const web3 = await getWeb3();
+
+      // Use web3 to get the user's accounts.
+      const accounts = await web3.eth.getAccounts();
+
+      // Get the contract instance.
+      const networkId = await web3.eth.net.getId();
+      const deployedNetwork = AllowlistContract.networks[networkId];
+      const instance = new web3.eth.Contract(
+        AllowlistContract.abi,
+        deployedNetwork && deployedNetwork.address
+      );
+
+      // Set web3, accounts, and contract to the state, and then proceed with an
+      // example of interacting with the contract's methods.
+      this.setState({ web3, accounts, contract: instance });
+    } catch (error) {
+      // Catch any errors for any of the above operations.
+      alert(
+        `Failed to load web3, accounts, or contract. Check console for details.`
+      );
+      console.error(error);
+    }
+  };
+
+  render() {
+    if (!this.state.web3) {
+      return <div>Loading Web3, accounts, and contract...</div>;
+    }
+    return (
+      <div className="App">
+        <AllowlistForm
+          contract={this.state.contract}
+          accounts={this.state.accounts}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
